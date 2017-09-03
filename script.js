439d36d96e4bc8297e39a92fa4d9bdf8a2e7928d
Rhyan Seymore <9959238@BCS.NET.NZ>
2017-09-04 11:55:55 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Describe what this program does
+alert("Hey mate. This program will compare two different numbers and display the greatest one");
+
+//Prompt user to enter a number and store it in a variable
+var number1 = prompt("Please enter your first number")
+
+//Pormpt user for another number and store it in a variable
+var number2 = prompt("Please enter your second number")
+
+//Using an "if else" statement, we will display the larger number of the two numbers given in an alert box
+if (number1 > number2)
+    {
+        alert("The first number you gave is WAY bigger than the second");
+    }
+    else 
+        {
+            alert("The second number you gave is HEEEEAPS higher than the first");
+        }
